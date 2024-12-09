ac5825f4f819a513599de581f0ea737817ee60f0
Augusto <augusto.perales.g@uni.pe>
2024-12-08 22:58:08 -0500

    rou

--- src/components/LoginPopup/LoginPopup.jsx
+++ src/components/LoginPopup/LoginPopup.jsx
@@ -31,8 +31,8 @@ const LoginPopup = ({ onClose }) => {
   const handleSubmit = async (e) => {
     e.preventDefault();
     const url = isLogin
-      ? "https://backend-stcom.vercel.app/api/auth/login"
-      : "https://backend-stcom.vercel.app/api/auth/register";
+      ? "https://backend-stcom.up.railway.app/api/auth/login"
+      : "https://backend-stcom.up.railway.app/api/auth/register";
 
     try {
       const response = await axios.post(url, formData);
