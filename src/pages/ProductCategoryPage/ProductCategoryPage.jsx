751d48a8279dc56c6fca932836b4b54ecbd64b73
Augusto <augusto.perales.g@uni.pe>
2024-12-09 20:31:13 -0500

    7

--- src/pages/ProductCategoryPage/ProductCategoryPage.jsx
+++ src/pages/ProductCategoryPage/ProductCategoryPage.jsx
@@ -1,12 +1,29 @@
 import { useParams, Link } from "react-router-dom";
-import { products } from "../../assets/assets";
 import "./ProductCategoryPage.css";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
 const ProductCategoryPage = () => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const fetchProducts = async () => {
+      try {
+        const { data } = await axios.get(
+          "https://backend-stcom.up.railway.app/api/products"
+        );
+        setProducts(data.products);
+      } catch (error) {
+        console.error("Error fetching products:", error);
+      }
+    };
+
+    fetchProducts();
+  }, []);
   const { categoryName } = useParams();
   console.log(`Categoría seleccionada: ${categoryName}`);
   const filteredProducts = products.filter(
-    (product) => product.product_category === categoryName
+    (product) => product.category === categoryName
   );
 
   return (
@@ -14,16 +31,13 @@ const ProductCategoryPage = () => {
       <h2>Productos de la categoría: {categoryName}</h2>
       <div className="product-list">
         {filteredProducts.map((product) => (
-          <div key={product.product_id} className="product-item">
-            <img src={product.product_image} alt={product.product_name} />
+          <div key={product.id} className="product-item">
+            <img src={product.image} alt={product.name} />
             <h3>
-              <Link to={`/product/${product.product_id}`}>
-                {product.product_name}
-              </Link>
+              <Link to={`/product/${product.id}`}>{product.name}</Link>
             </h3>
-            <p>{product.product_desc}</p>
-            <p>${product.product_price}</p>
-            {product.is_on_sale && <span>En oferta</span>}
+            <p>${product.price}</p>
+            {product.isOnsale && <span>En oferta</span>}
           </div>
         ))}
       </div>
